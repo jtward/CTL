c121c89963e52ae0595ba50bc5664bdc2c22f181
James Ward <jtward.is@gmail.com>
2016-02-07 22:48:54 +0100

    refactoring

--- src/CTLTokenizer.js
+++ src/CTLTokenizer.js
@@ -1,4 +1,4 @@
-import {includes} from 'lodash';
+import {includes, partial} from 'lodash';
 
 var Token = function(type, value) {
 	return {
@@ -14,8 +14,9 @@ var SyntaxError = function(message) {
 	};
 };
 
-var prefix = '-';
-var suffix = '>';
+var dash = '-';
+var rightAngleBracket = '>';
+
 var nonOpRegex = /^(?!^.*[AEXFGURW]+.*$)[\w\d\u0391-\u03C9\u00C0-\u00FF]+/;
 var isNonOp = function(character) {
 	return nonOpRegex.test(character);
@@ -27,13 +28,20 @@ var isOp = function(character) {
 };
 
 var whitespaceRegex = /^\s$/;
+var isWhitespaceChar = function(c) {
+	return whitespaceRegex.test(c);
+};
+
+var isBooleanChar = partial(includes, ['T', 'F']);
+
+var stringDelimiterRegex = /^['"]$/;
+var isStringDelimiterChar = function(c) {
+	return stringDelimiterRegex.test(c);
+};
 
 export default function(input) {
 	var c = 0, // The current character.
-		from = 0, // The index of the start of the token.
 		i = 0, // The index of the current character.
-		length = input.length,
-		n, // The number value.
 		q, // The quote character.
 		str, // The string value.
 		result = [];
@@ -45,14 +53,14 @@ export default function(input) {
 
 	c = input.charAt(i);
 	while(c) {
-		from = i;
 		// Ignore whitespace.
-		if (whitespaceRegex.test(c)) {
+		if (isWhitespaceChar(c)) {
 			i += 1;
 			c = input.charAt(i);
 		}
+
 		//strings (delimited by either ' or ") can contain anything except backslashes.
-		else if (c === '"' || c === '\'') {
+		else if (isStringDelimiterChar(c)) {
 			str = '';
 			q = c;
 			//skip over the opening string delimiter
@@ -77,16 +85,15 @@ export default function(input) {
 			c = input.charAt(i);
 		}
 
-		//the non-operators '\T' and '\F' are special: they mean 'true' and 'false' respectively.
+		// the non-operators '\T' and '\F' are special: they mean 'true' and 'false' respectively.
 		// although 'T' would also be unambiguous, '\T' screams 'i'm special!', so there
 		// is no way that \T could be confused with an ordinary property. F is an operator, so
 		// we need to be able to tell F from \F (although the user could always specify !\T).
 		else if(c === '\\') {
-			var specials = ['T', 'F'];
-			str = '\\';
+			str = c;
 			i += 1;
 			c = input.charAt(i);
-			if(includes(specials, c)) {
+			if(isBooleanChar(c)) {
 				str += c;
 				i += 1;
 				result.push(Token('atom', str));
@@ -94,6 +101,27 @@ export default function(input) {
 			}
 			else {
 				throw SyntaxError('Unknown character: \'\\\'');
+			}
+		}
+
+		else if(isOp(c)) {
+			i += 1;
+			result.push(Token('operator', c));
+			c = input.charAt(i);
+		}
+
+		// dashes must be followed immediately by right angle brackets
+		else if(c === dash) {
+			str = c;
+			i += 1;
+			if(c === rightAngleBracket) {
+				str += c;
+				i += 1;
+				result.push(Token('operator', str));
+				c = input.charAt(i);
+			}
+			else {
+				throw SyntaxError('Unknown character: \'-\'');
 			}
 		}
 		
@@ -115,42 +143,6 @@ export default function(input) {
 			c = input.charAt(i);
 		}
 		
-		//prefix character (i.e. '-'; look for '>' next)
-		else if(prefix.indexOf(c) >= 0) {
-			str = c;
-			i += 1;
-			while(i < length) {
-				c = input.charAt(i);
-				if(suffix.indexOf(c)) {
-					break;
-				}
-				str += c;
-				i += 1;
-			}
-			result.push(Token('operator', str));
-			c = input.charAt(i);
-		}
-		
-		//prefix character (i.e. '-'; look for '>' next)
-		else if(prefix.indexOf(c) >= 0) {
-			str = c;
-			i += 1;
-			while(i < length) {
-				c = input.charAt(i);
-				if(suffix.indexOf(c) >= 0) {
-					break;
-				}
-				str += c;
-				i += 1;
-			}
-			result.push(Token('operator', str));
-			c = input.charAt(i);
-		}
-		else if(isOp(c)) {
-			i += 1;
-			result.push(Token('operator', c));
-			c = input.charAt(i);
-		}
 		else {
 			throw SyntaxError('Unknown character: \''+input.charAt(i)+'\'');
 		}
