ea9a0c91c5873e2cf673750a4a0b7cfa667328e9
James Ward <jtward.is@gmail.com>
2016-02-27 15:46:16 +0100

    refactor tokenizer

--- src/CTLTokenizer.js
+++ src/CTLTokenizer.js
@@ -1,126 +1,72 @@
-const Token = (type, value) => {
-	return {
-		type,
-		value
+import { each, map } from 'lodash';
+
+const rules = [{
+	// whitespace
+	regex: /^\s*/
+}, {
+	// unquoted atoms
+	regex: /^(((?![FAUXGREW])[\w\d\u0391-\u03C9\u00C0-\u00FF])+)/,
+	type: 'atom'
+}, {
+	// quoted atoms
+	regex: /^(['"])(.*?)\1/,
+	valueCaptureGroup: 2,
+	type: 'atom'
+}, {
+	// boolean literals
+	regex: /^\\[TF]/,
+	type: 'atom'
+}, {
+	// operators
+	regex: /^[FAUXGREW&|!()]|->/,
+	type: 'operator'
+}];
+
+// turn the config into functions which create tokens from input
+const tokenizers = map(rules, (rule) => {
+	return (input, appendToken, skip) => {
+		const matches = rule.regex.exec(input);
+		if (matches) {
+			if (rule.type) {
+				appendToken(rule.type, matches[rule.valueCaptureGroup || 0]);
+			}
+			skip(matches[0].length);
+		}
 	};
-};
-
-const SyntaxError = (message) => {
-	return {
-		name: 'SyntaxError',
-		message
-	};
-};
-
-const backslash = '\\';
-const dash = '-';
-const rightAngleBracket = '>';
-const startAtom = /^(?![FAUXGREW])[\w\d\u0391-\u03C9\u00C0-\u00FF]/;
-const atom = /^(((?![FAUXGREW])[\w\d\u0391-\u03C9\u00C0-\u00FF])*)/;
-const operator = /^[FAUXGREW&|!()]$/;
-const whitespace = /^\s$/;
-const str = /^(['"])([^\\]*?)\1/;
-
-const isBoolean = (c) => {
-	return (c === 'T') || (c === 'F');
-};
-
-const isStringDelimiter = (c) => {
-	return (c === '\'') || (c === '"');
-};
-
-const tokenizeString = (delimiter, input) => {
-	const matches = str.exec(input);
-	if (matches) {
-		return matches[2];
-	}
-	else if (input.indexOf(delimiter) !== -1) {
-		throw SyntaxError('Backslashes are not allowed in strings!');
-	}
-	else {
-		throw SyntaxError(`Expected ${q} but found end of input.`);
-	}
-};
-
-const tokenizeAtom = (input) => {
-	const matches = atom.exec(input);
-	return matches[1];
-};
+});
 
 export default (input) => {
-	const result = [];
-	
-	if (input) {
-		let c;
+	const output = [];
+	let didTokenize = false;
 
-		const { skip, rest } = (() => {
-			let i = 0;
-			c = input.charAt(i);
-			return {
-				skip: (n = 1) => {
-					i += n;
-					c = input.charAt(i);
-				},
-				rest: () => {
-					return input.slice(i);
-				}
+	const appendToken = (type, value) => {
+		didTokenize = true;
+		output.push({
+			type,
+			value
+		});
+	};
+
+	const skip = (n = 1) => {
+		didTokenize = didTokenize || n > 0;
+		input = input.slice(n);
+	};
+
+	while (input) {
+		didTokenize = false;
+
+		each(tokenizers, (rule) => {
+			rule(input, appendToken, skip);
+			return !didTokenize; // break out if we tokenized anything
+		});
+
+		if (!didTokenize && input) {
+			throw {
+				name: 'SyntaxError',
+				message: `Unknown character: '${input[0]}'.`
 			};
-		})();
-
-		while (c) {
-			// Ignore whitespace.
-			if (whitespace.test(c)) {
-				skip();
-			}
-
-			else if (isStringDelimiter(c)) {
-				const str = tokenizeString(c, rest());
-				result.push(Token('atom', str));
-				skip(str.length + 2); // add 2 for the delimiters, which are not part of the string
-			}
-
-			// the non-operators '\T' and '\F' are special: they mean 'true' and 'false' respectively.
-			// although 'T' would also be unambiguous, '\T' screams 'i'm special!', so there
-			// is no way that \T could be confused with an ordinary property. F is an operator, so
-			// we need to be able to tell F from \F (although the user could always specify !\T).
-			else if (c === backslash) {
-				skip(); // over backslash
-				if (isBoolean(c)) {
-					result.push(Token('atom', `\\${c}`));
-					skip();
-				}
-				else {
-					throw SyntaxError('Unknown character: \'\\\'');
-				}
-			}
-
-			else if (operator.test(c)) {
-				result.push(Token('operator', c));
-				skip();
-			}
-
-			// dashes must be followed immediately by right angle brackets
-			else if (c === dash) {
-				skip();
-				if (c === rightAngleBracket) {
-					result.push(Token('operator', '->'));
-					skip();
-				}
-				else {
-					throw SyntaxError('Unknown character: \'-\'.');
-				}
-			}
-
-			else if (startAtom.test(c)) {
-				const atom = tokenizeAtom(rest());
-				skip(atom.length);
-				result.push(Token('atom', atom));
-			}
-
-			else {
-				throw SyntaxError(`Unknown character: '${input.charAt(i)}'.`);
-			}
 		}
 	}
-	return result;
+
+	return output;
 };
