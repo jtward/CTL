e8119978690d15bab40a891fd7cd91bf713a4b54
James Ward <jtward.is@gmail.com>
2016-06-28 21:29:22 +0200

    remove unneeded dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,6 @@
 import { rollup } from 'rollup';
-import nodeResolve from 'rollup-plugin-node-resolve';
 
 export default {
 	entry: 'src/CTL.js',
-	plugins: [
-		nodeResolve({ jsnext: true })
-	],
 	dest: 'lib/CTL.js'
 };
